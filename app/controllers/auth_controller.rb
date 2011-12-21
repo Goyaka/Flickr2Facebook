e5b7049d344b700c9138d18f40d0e056a9d09721
Surendran Mahendran <surenspost@gmail.com>
2011-12-22 02:41:16 +0530

    Enable Flick Authentication

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,19 +5,24 @@ class AuthController < ApplicationController
   end
 
   def facebook_authenticate
-    session[:at]=nil
+    session[:at] = nil
     facebook_scope = 'offline_access,publish_stream,user_photos,user_photo_video_tags'
     redirect_to facebook_authenticator.authorize_url(:scope => facebook_scope, :display => 'page')
   end
 
   def facebook_auth
     redirect_to :action => 'facebook_authenticate' and return unless session[:at]
-    user = Mogli::User.find("me",Mogli::Client.new(session[:at]))
-    @user = user
     redirect_to :controller => 'application', :action => 'main'
   end
   
   def flickr_auth
+    config = YAML.load_file(Rails.root.join("config/flickr.yml"))[Rails.env]
+    FlickRaw.api_key = config['app_id']
+    FlickRaw.shared_secret = config['shared_secret']
+    
+    token = flickr.get_request_token :oauth_callback => url_for(:action => "flickr_callback")
+    auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'read')
+    redirect_to auth_url 
   end
 
   def facebook_callback
@@ -27,5 +32,7 @@ class AuthController < ApplicationController
   end
   
   def flickr_callback
+    @verifier = params[:oauth_verifier]
+    @token = params[:oauth_token]
   end
 end
