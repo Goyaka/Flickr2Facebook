9e5e169ab31b5a03f2fc2e587a93967390fd8d9e
Surendran Mahendran <surenspost@gmail.com>
2011-12-22 04:58:57 +0530

    Main page just shows content from database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,22 +8,15 @@ class ApplicationController < ActionController::Base
   
   def main
     # try to get the FB user handle
-    @fb_user = Mogli::User.find("me",Mogli::Client.new(session[:at])) if session[:at]
+    facebook_user = Mogli::User.find("me",Mogli::Client.new(session[:at])) if session[:at]
 
-    if @fb_user
-      @user = User.where(:user => @fb_user.username)[0]
-      
-      # if the access token is defined, get the flickr user as well.
-      if @user.flickr_access_token
-        config = YAML.load_file(Rails.root.join("config/flickr.yml"))[Rails.env]
-        FlickRaw.api_key = config['app_id']
-        FlickRaw.shared_secret = config['shared_secret']    
-        
-        flickr.access_token = @user.flickr_access_token
-        flickr.access_secret = @user.flickr_access_secret
-        
-        @flickr_user = flickr.test.login
+    if facebook_user
+      @user = User.where(:user => facebook_user.username)[0]
+      if @user
+        @fb_user = @user.user
+        @flickr_user = @user.flickr_username
       end
+      
     end
   end
 end
