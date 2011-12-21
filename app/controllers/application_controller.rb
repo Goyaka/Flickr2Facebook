dd8e0a8913203b70be5921274d6fe6652e4eefda
Surendran Mahendran <surenspost@gmail.com>
2011-12-22 04:36:24 +0530

    Fix main view to handle multiple login cases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,23 @@ class ApplicationController < ActionController::Base
   end
   
   def main
+    # try to get the FB user handle
     @fb_user = Mogli::User.find("me",Mogli::Client.new(session[:at])) if session[:at]
-    puts @fb_user
+
+    if @fb_user
+      @user = User.where(:user => @fb_user.username)[0]
+      
+      # if the access token is defined, get the flickr user as well.
+      if @user.flickr_access_token
+        config = YAML.load_file(Rails.root.join("config/flickr.yml"))[Rails.env]
+        FlickRaw.api_key = config['app_id']
+        FlickRaw.shared_secret = config['shared_secret']    
+        
+        flickr.access_token = @user.flickr_access_token
+        flickr.access_secret = @user.flickr_access_secret
+        
+        @flickr_user = flickr.test.login
+      end
+    end
   end
 end
