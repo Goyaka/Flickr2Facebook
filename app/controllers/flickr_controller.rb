11e39a5439a8834523ee16aa17aeadb8dafccb1e
Surendran Mahendran <surenspost@gmail.com>
2011-12-22 18:37:32 +0530

    Ability to add sets to be imported

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -12,11 +12,32 @@ class FlickrController < ApplicationController
       @user = User.where(:user => facebook_user.username)[0]
       flickr.access_token = @user.flickr_access_token
       flickr.access_secret = @user.flickr_access_secret
-      login = flickr.test.login()
-      @sets = flickr.photosets.getList(:user_id => login['id'])
+      @sets = flickr.photosets.getList(:user_id => @user.flickr_user_nsid)
     end
     
     response = { :sets => @sets }
     render :json => response
   end
+  
+  def select_sets
+    # TODO: Make the config loading part separated
+    config = YAML.load_file(Rails.root.join("config/flickr.yml"))[Rails.env]
+    FlickRaw.api_key = config['app_id']
+    FlickRaw.shared_secret = config['shared_secret']
+
+    flickr = FlickRaw::Flickr.new
+    facebook_user = Mogli::User.find("me",Mogli::Client.new(session[:at]))
+    if facebook_user
+      @user = User.where(:user => facebook_user.username)[0]
+      if @user
+        params["set"].each do |set| 
+          photoset = Photoset.new(:user_id => @user, :photoset => set, :status => 'false')
+          photoset.save!
+        end
+      end
+    end
+    response = { :success => true }
+    render :json => response
+  end
+
 end
