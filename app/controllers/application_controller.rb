17db7aefcca7c722be83bd043c53af5fc37e601c
Surendran Mahendran <surenspost@gmail.com>
2011-12-22 07:08:09 +0530

    Handle Mogli exception properly in main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,17 @@ class ApplicationController < ActionController::Base
   
   def main
     # try to get the FB user handle
-    facebook_user = Mogli::User.find("me",Mogli::Client.new(session[:at])) if session[:at]
+    begin 
+      facebook_user = Mogli::User.find("me",Mogli::Client.new(session[:at])) if session[:at]
+    rescue Mogli::Client::HTTPException
+      session[:at] = nil
+      redirect_to :controller => 'auth', :action => 'facebook_auth'
+    end
 
     if facebook_user
       @user = User.where(:user => facebook_user.username)[0]
-      if @user
-        @fb_user = @user.user
-        @flickr_user = @user.flickr_username
-      end
-      
+      @fb_user = @user.user
+      @flickr_user = @user.flickr_username
     end
   end
 end
