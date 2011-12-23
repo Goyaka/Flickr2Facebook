2e35daf7a96f37ce3ee46a47b0efc811e6c9000c
alagu <allagappan@gmail.com>
2011-12-23 12:46:04 +0530

    Process one photo at a time.

--- script/worker.rb
+++ script/worker.rb
@@ -4,29 +4,31 @@ class Worker < ActiveRecord::Base
   
   def self.upload_loop
     while true
-      sleep 1
-       unprocessed_photos = Photo.where("status = ?", FlickrController::PHOTO_NOTPROCESSED)
-       unprocessed_photos.each do |photo|
-         photoset = Photoset.find(photo.photoset_id)
-         user     = User.find(photoset.user_id)
-         job      = Job.new(user.fb_session, user.flickr_oauth_token, user.flickr_oauth_secret)              
-         job.upload(photo)
-       end
-       break
+      photo = Photo.where("status = ?", FlickrController::PHOTO_NOTPROCESSED).first
+      if photo
+        photoset = Photoset.find(photo.photoset_id)
+        user     = User.find(photoset.user_id)
+        job      = Job.new(user.fb_session, user.flickr_oauth_token, user.flickr_oauth_secret)              
+        job.upload(photo)
+      else
+        # "No photo. waiting."
+        sleep 1
+      end
     end
   end
   
   def self.split_sets_loop
     while true
-      sleep 1
-      unprocessed_sets = Photoset.where("status = ?", FlickrController::PHOTOSET_NOTPROCESSED)
-      unprocessed_sets.each do |set| 
-        puts "Doing set " + set.photoset
+      set  = Photoset.where("status = ?", FlickrController::PHOTOSET_NOTPROCESSED).first
+      if set
+        puts "Splitting set " + set.photoset + " to photos"
         user = User.find(set.user_id)
         job  = Job.new(user.fb_session, user.flickr_oauth_token, user.flickr_oauth_secret)
         job.upload_set(set.photoset)
+      else
+        # "No photosets. waiting."
+        sleep 1
       end
-      break
     end
   end
 end
