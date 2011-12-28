fd5b6910153dfd6f5a9843bf82a97b2476b48b1e
Rajagopal Natarajan <rajagopal.n@gmail.com>
2011-12-28 17:38:29 +0530

    Mailer uses SES instead of pony

--- app/models/async_mailer_job.rb
+++ app/models/async_mailer_job.rb
@@ -1,16 +1,14 @@
-require 'pony'
-
 class AsyncMailerJob
   def initialize(mailcontent)
     @content = mailcontent
+    @mailer = AmazonSes::Mailer.new(:access_key => "AKIAICVVEBIXIX3FBQFQ", :secret_key => "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")
   end
 
   def perform
-    Pony.mail(
-      :to => @content[:recipient], 
-      :from => @content[:sender], 
-      :subject => @content[:subject], 
-      :body => @content[:body])
+    mailer.deliver to:      @content[:recipient],
+               from:    @content[:sender],
+               subject: @content[:subject],
+               body:    @content[:body]
   end
   
 end
