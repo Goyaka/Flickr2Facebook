1e8462a6dfd7fd5fd543dcd686f25052d278e2eb
alagu <allagappan@gmail.com>
2012-01-05 10:25:05 +0530

    Get album info in photo set code

--- app/models/photoset.rb
+++ app/models/photoset.rb
@@ -1,4 +1,22 @@
 class Photoset < ActiveRecord::Base
   belongs_to :users
   has_many :photos
+  
+  def get_album_info
+    if self[:source] == Constants::SOURCE_PICASA
+      user       = User.find(self[:user_id])
+      albuminfo  = user.get_picasa_album_info(self[:photoset])
+      albumname  = albuminfo['title'][0]['content']
+      albumdesc  = ''
+      photocount = (albuminfo['entry'].length)
+    elsif self[:source] == Constants::SOURCE_FLICKR
+      setinfo     = flickr.photosets.getInfo(:photoset_id => photoset_id)
+      albumname   = setinfo.title
+      albumdesc = setinfo.description
+      photocount  = photos.length
+    end
+    
+    return albumname, albumdesc, photocount
+    
+  end
 end
