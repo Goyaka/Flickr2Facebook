26e67e58a28a2a5e8775802bec4dcda4309ea41b
UnifyPhotos <app_user@Unify.(none)>
2012-06-09 20:43:39 +0000

    Working Fb and Flickr Authentication

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,11 +1,16 @@
 require 'flickraw-cached'
+require 'rubygems'
 
 class FlickrController < ApplicationController  
   def get_sets_notuploaded
-    user = User.where(fb_session =>session[:at]).first
+    user = User.where(:fb_session =>session[:at]).first
     sets = user.get_all_flickr_sets
 
-    existing_sets = Photoset.select('photoset').where('user_id = ? and source = ?', user, Constants::SOURCE_FLICKR).map {|set| set.photoset}.compact
+#    existing_sets = Photoset.select('photoset').where('user_id = ? and source = ?', user, Constants::SOURCE_FLICKR).map {|set| set.photoset}.compact
+
+
+    existing_sets = Photoset.where(:user_id => user.id,:source => Constants::SOURCE_FLICKR).only('photoset').map{|set| set.photoset}.compact
+
 
     ret_sets = []
     for set in sets
