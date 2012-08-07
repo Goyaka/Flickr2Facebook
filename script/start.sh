fd57c5f8212472081dd7e0795eb7523b10896aeb
elricL <Sriram.ga@directi.com>
2012-08-07 19:09:17 +0530

    Changed Production Config for beanstalk and mongoid

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,6 @@
-cd /opt/Flickr2Facebook
-sudo nohup rails runner -e production "Worker.split_sets_loop" > ./log/split_log &
-sudo nohup rails runner -e production "Worker.upload_loop" > ./log/upload1_log &
-sudo nohup rails runner -e production "Worker.upload_loop" > ./log/upload2_log &
-sudo nohup rails runner -e production "Worker.upload_loop" > ./log/upload3_log &
-sudo nohup rails runner -e production "Worker.upload_loop" > ./log/upload4_log &
+cd ..
+rails runner -e production "Worker.split_sets_loop" > ./log/split_log &
+rails runner -e production "Worker.upload_loop" > ./log/upload1_log &
+rails runner -e production "Worker.upload_loop" > ./log/upload2_log &
+rails runner -e production "Worker.upload_loop" > ./log/upload3_log &
+rails runner -e production "Worker.upload_loop" > ./log/upload4_log &
